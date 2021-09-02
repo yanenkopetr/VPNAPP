3b5d611f20aaada73a1ce0636e07970dc35f89d4
Yanenko Petr <yanenkopetr1481@gmail.com>
2021-09-02 16:06:35 +0300

    init commit

---
+++ VPNAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VPNAPP
+//
+//  Created by Petr Yanenko on 02.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
